e653f76c50ec8634bc279aec7c54540eddab6328
Artem Orobets <enisher@gmail.com>
2013-09-18 14:30:00 +0300

    CreateNetworkCommand: Loading of xUser

--- src/main/java/org/ndexbio/rest/CreateNetworkPostCommand.java
+++ src/main/java/org/ndexbio/rest/CreateNetworkPostCommand.java
@@ -1,15 +1,22 @@
 package org.ndexbio.rest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.id.OClusterPositionFactory;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.id.ORecordId;
 import com.orientechnologies.orient.server.config.OServerCommandConfiguration;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpRequest;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpResponse;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpUtils;
 import com.orientechnologies.orient.server.network.protocol.http.command.OServerCommandAuthenticatedDbAbstract;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
 
 public class CreateNetworkPostCommand extends OServerCommandAuthenticatedDbAbstract {
   private static final String[]    NAMES = { "POST|ndexNetworkCreate/*" };
@@ -34,8 +41,10 @@ public class CreateNetworkPostCommand extends OServerCommandAuthenticatedDbAbstr
 
       final ObjectMapper objectMapper = new ObjectMapper();
 
-      JsonNode rootNode = objectMapper.readTree(iRequest.content);
-      ndexNetworkService.createNetwork(null, rootNode, orientGraph);
+      final JsonNode rootNode = objectMapper.readTree(iRequest.content);
+      final JsonNode network = rootNode.get("network");
+      final OrientVertex owner = loadOwner(orientGraph, rootNode.get("accountid").asText());
+      ndexNetworkService.createNetwork(owner, network, orientGraph);
 
       orientGraph.commit();
     } finally {
@@ -45,6 +54,25 @@ public class CreateNetworkPostCommand extends OServerCommandAuthenticatedDbAbstr
     iResponse.send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_TEXT_PLAIN, "Network created",
         null, true);
     return false;
+  }
+
+  private OrientVertex loadOwner(OrientGraph orientGraph, String accountid) {
+    final OrientVertex vertex = orientGraph.getVertex(convertToRID(accountid));
+
+    if (!vertex.getLabel().equals("xUser")) {
+      throw new RuntimeException("User with id = " + accountid + " is not found.");
+    }
+
+    return vertex;
+  }
+
+  private ORID convertToRID(String id) {
+    final Matcher m = Pattern.compile("^C(\\d*)R(\\d*)$").matcher(id.trim());
+
+    if (m.matches())
+      return new ORecordId(Integer.valueOf(m.group(1)), OClusterPositionFactory.INSTANCE.valueOf(m.group(2)));
+    else
+      throw new RuntimeException(id + " is not valid jid");
   }
 
   @Override
